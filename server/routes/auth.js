ac04c13e164be762e3dfe1d3621b6e5b65e67cd6
JackieCisneros <krieger.jacqueline@gmail.com>
2023-10-02 10:11:55 -0600

    comments for routes in express

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,38 +3,43 @@ const router = express.Router();
 const bcrypt = require('bcrypt')
 const users = require('../models/User')
 
-// Login route
+//function that returns a promise, using await to findOne by email and also await to 
+//compare with bcrypt password passed in and password in user table
+
+//two objects are required in express: request (req incoming) and response (resp outgoing)
+    //req contains info about the HTTP request that was made from react; here is email and password
+    //res sends back HTTP to client to set resp headers, etc
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-      console.log('recieved login request', {email, password})
-      const user = await users.findOne({ email });
+        console.log('recieved login request', {email, password})
+        const user = await users.findOne({ email });
     
-      if (!user) {
-        return res.status(401).json({ message: 'User not found' });
-      } 
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        } 
     
-      const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(password, user.password);
     
-      if (passwordMatch) {
-        console.log('Authentication successful');
-        req.session.userId = user._id;
-        req.session.isAuthenticated = true;
+        if (passwordMatch) {
+            console.log('Authentication successful');
+            req.session.userId = user._id;
+            req.session.isAuthenticated = true;
 
-        res.status(200).json({ message: `Login successful, session_id is ${req.session.userId}` });
-      } else {
-        console.log('Authentication failed');
-        res.status(401).json({ message: 'Incorrect password' });
-      }
+            res.status(200).json({ message: `Login successful, session_id is ${req.session.userId}` });
+        } else {
+            console.log('Authentication failed');
+            res.status(401).json({ message: 'Incorrect password' });
+        }
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
     }
 });
 
 
-router.post('/logout', async (req, res) => {
+router.post('/logout', (req, res) => {
     if(req.session){
         req.session.destroy(err=>{
             if(err){
